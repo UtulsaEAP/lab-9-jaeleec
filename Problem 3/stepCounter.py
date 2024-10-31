c704202d609a8e98839361469623b926d4469d0a
jaeleec <jqc2435@utulsa.edu>
2024-10-31 08:35:00 -0500

    did prob 3 but not passing test cases

--- Problem 3/stepCounter.py
+++ Problem 3/stepCounter.py
@@ -1,10 +1,11 @@
 def feet_to_steps(user_feet):
    #write your code here
-   return
+   return(user_feet/2.5)
 
 if __name__ == '__main__':
     #take input feet steps here
+    user_feet=float(input())
     #store it into the function
-    
+    steps = feet_to_steps(user_feet)
     #print your steps here
-    feet_to_steps(5280)
+    print(int(steps))
