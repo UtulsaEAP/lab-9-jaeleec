1371666a09124dda715d08354a82c9ade825b41e
jaeleec <jqc2435@utulsa.edu>
2024-11-07 08:30:26 -0600

    did prob 6

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,9 +1,8 @@
 def fibonacci(n):
-    i = 1
-    while i < n: 
-        i = i + i+1
-    n = i
-    return(n)
+    if n <=1:
+       return n
+    else: 
+        return (fibonacci(n-1) + fibonacci(n-2))
 
 if __name__ == '__main__':
     start_num = int(input())
